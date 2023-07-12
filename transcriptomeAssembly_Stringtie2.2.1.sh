6291ba1027689440475188e20d8c96bb6abb60bc
Akram Mendez <akramsharim@gmail.com>
2023-07-11 19:16:31 -0600

    Added comments to transcriptomeAssembly_Stringtie2.2.1.sh

---
+++ transcriptomeAssembly_Stringtie2.2.1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+#SBATCH -A snic2022-22-85
+#SBATCH -p core -n 8
+#SBATCH -t 24:00:00
+#SBATCH -J Stringtie2
+
+#This scripts performs a de novo transcriptome assembly from Oxford Nanopore dRNA long-read sequencing using StringTie2
+#https://github.com/gpertea/stringtie
+
+module load bioinfo-tools
+
+stringtie="/domus/h1/amendez/private/stringtie/stringtie"
+
+inputdir=$(realpath $1) #Path to input BAM files from dRNA reads mapped to T2T reference genome
+outdir=$(realpath $2) # Output directory
+
+cd ${SNIC_TMP}
+
+echo "Copying BAM files to ${SNIC_TMP}"
+
+cp ${inputdir}/*.bam .
+cp ${inputdir}/*.bai .
+
+for bam in $(ls *.bam)
+do
+    echo "Performing de novo transcriptome assembly with StringTie2 for ${bam}"
+    outfile=$(echo "${bam%.bam}_txm_Stringtie2.2.1.gtf")
+    ${stringtie} -L -o ${outfile} ${bam} -A -v
+done
+
+
+echo "Copying assembled files to ${outdir}"
+cp *.gtf ${outdir}
+cp *.tab ${outdir}
+
+echo "Done."
